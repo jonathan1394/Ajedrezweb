3eb5ba48dfb97d0ab7a88d917b33f33e260c5f58
Jonathan Machado <jonhy.jona@gmail.com>
2021-04-17 04:19:39 +0200

    otro mas

---
+++ Ajedrez/Tablero.h
@@ -0,0 +1,10 @@
+#ifndef TABLERO_H_INCLUDED
+#define TABLERO_H_INCLUDED
+
+typedef struct Tablero{
+        int x;
+        int y;
+}
+
+
+#endif // TABLERO_H_INCLUDED
--- Ajedrez/main.cpp
+++ Ajedrez/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "Tablero.h"
 using namespace std;
 
 int main()
